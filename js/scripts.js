e1d5e8c33d4bc63d24b67b9445059266f42bc5a6
Mostafa Naghipoor <89272100+MostafaNaghipoor82@users.noreply.github.com>
2022-09-21 15:47:34 -0700

    I created a loop to console.log the pokemon name.

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,6 @@ let pokemonList = [];
 pokemonList.push({name:"Vanillite", height: "1.04m", type: "water"});
 pokemonList.push({name:"Klink", height: "1.5m", type: "bug"});
 pokemonList.push({name:"Deino", height: "0.5m", type: "flying"});
+for(let i = 0; i<pokemonList.length; i++){
+    console.log(pokemanList[i].name);
+} 
