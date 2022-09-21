03e094266290f38b11886108e44c150ac885bbfd
Mostafa Naghipoor <89272100+MostafaNaghipoor82@users.noreply.github.com>
2022-09-20 21:21:36 -0700

    defined pokemon list variable and added three pokemons to the list

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
-alert('Hello world');
-let favoriteFood= "sushi";
-document.write(favoriteFood); 
+let pokemonList = [];
+pokemonList.push({name:"Vanillite", height: "1.04m", type: "water"});
+pokemonList.push({name:"Klink", height: "1.5m", type: "bug"});
+pokemonList.push({name:"Deino", height: "0.5m", type: "flying"});
